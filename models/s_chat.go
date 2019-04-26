9552099c12275742c6c208da77b6272d241ad188
fushihao <fushihao@mifengkong.cn>
2019-04-26 14:26:50 +0800

    -

--- models/s_chat.go
+++ models/s_chat.go
@@ -3,7 +3,7 @@ package models
 type SChat struct {
 	Id         int    `json:"id" xorm:"not null pk autoincr unique INT(11)"`
 	Message    string `json:"message" xorm:"not null default '' VARCHAR(255)"`
-	FromId     int    `json:"form_id" xorm:"not null default 0 INT(11)"`
+	FromId     int    `json:"from_id" xorm:"not null default 0 INT(11)"`
 	ToId       int    `json:"to_id" xorm:"not null default 0 INT(11)"`
 	ReadStatus int    `json:"read_status" xorm:"not null default 0 TINYINT(1)"`
 	CreateTime int    `json:"create_time" xorm:"not null default 0 INT(11)"`
